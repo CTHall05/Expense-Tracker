6c43851345a7e6ee4e8e042b38f6db7947a7ee27
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-03-06 02:51:41 +0000

    Created a Context folder to manage the context of the data within the app through useContext and useReducer

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -3,6 +3,7 @@ import { Header } from './Components/Header.js';
 import { Balance } from './Components/Balance.js'; 
 import { IncomeExpenses } from './Components/IncomeExpenses.js';
 import { TransactionList } from './Components/TransactionList.js';
+import { AddTransaction } from './Components/AddTransaction.js';
 import './App.css';
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
     <div>
       <Header></Header>
       <div className="container">
-        <Balance></Balance>
-        <IncomeExpenses></IncomeExpenses>
-        <TransactionList></TransactionList>
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
       </div>
     </div>
   );
--- expense-tracker/src/Components/AddTransaction.js
+++ expense-tracker/src/Components/AddTransaction.js
@@ -10,11 +10,11 @@ export const AddTransaction = () => {
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value= {text} id="text" placeholder="Enter text..."/>
+          <input type="text" value= {text} onChange={(event) => setText(event.target.value)} id="text" placeholder="Enter text..."/>
         </div>
         <div class="form-control">
           <label htmlFor="amount">Amount <br /> (negative - expense, positive - income)</label>
-          <input type="number" id='amount' placeholder='Enter amount...' />
+          <input type="number" value={amount} onChange={(event) => setAmount(event.target.value)} placeholder='Enter amount...' />
         </div>
         <button className="btn">Add Transaction</button>
       </form>
